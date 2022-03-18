98e48efc2bfe313d7508a57b6d089993ba100165
Diandra Irdianti <diandrair@Diandras-MacBook-Pro.local>
2022-03-18 10:34:46 +0700

    add player

---
+++ src/main/java/com/nostraAcademy/NASnakeLadder/Player.java
@@ -0,0 +1,19 @@
+package com.nostraAcademy.NASnakeLadder;
+
+public class Player {
+    String name;
+    int position;
+
+    public String getName() {
+        return name;
+    }
+    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Player() {
+        
+    }
+}
